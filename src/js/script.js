1bd07e1c1a3bd7b165cfb9c8381532bf70e8eb4e
Ana María <anamariasosam@gmail.com>
2018-03-26 18:16:13 -0500

    create menu style

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+function openNav() {
+  $("#myNav").css( "height", "100%" )
+
+  $(".menu-content--item").removeClass('zoomOutLeft')
+  $(".menu-content--item").addClass('fadeInLeftBig')
+}
+
+function closeNav() {
+  $(".menu-content--item").removeClass('fadeInLeftBig')
+  $(".menu-content--item").addClass('zoomOutLeft')
+
+  setTimeout(function(){
+    $("#myNav").css( "height", "0%" )
+  }, 500);
+}
+
+function nav() {
+  $('.menu--btn').toggleClass('open');
+
+  if($('.menu--btn').hasClass("open")) {
+    openNav()
+  }else{
+    closeNav()
+  }
+}
+
+$(document).ready(function(){
+	$('.menu--btn').click(nav);
+});
