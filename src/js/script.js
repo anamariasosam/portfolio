b35aa598855e6b2e5a43fa0b6c022f7c66dc4267
Ana María <anamariasosam@gmail.com>
2018-03-28 17:07:36 -0500

    Remove scroll plugin

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,23 @@
 function openNav() {
-  $("#myNav").css( "height", "100%" )
+  $('#myNav').css( 'height', '100%' )
 
-  $(".menu-content--item").removeClass('zoomOutLeft')
-  $(".menu-content--item").addClass('fadeInLeftBig')
+  $('.menu-content--item').removeClass('zoomOutLeft')
+  $('.menu-content--item').addClass('fadeInLeftBig')
 }
 
 function closeNav() {
-  $(".menu-content--item").removeClass('fadeInLeftBig')
-  $(".menu-content--item").addClass('zoomOutLeft')
+  $('.menu-content--item').removeClass('fadeInLeftBig')
+  $('.menu-content--item').addClass('zoomOutLeft')
 
   setTimeout(function(){
-    $("#myNav").css( "height", "0%" )
+    $('#myNav').css( 'height', '0%' )
   }, 500);
 }
 
 function nav() {
   $('.menu--btn').toggleClass('open');
 
-  if($('.menu--btn').hasClass("open")) {
+  if($('.menu--btn').hasClass('open')) {
     openNav()
   }else{
     closeNav()
@@ -25,10 +25,13 @@ function nav() {
 }
 
 $(document).ready(function(){
-	$('.menu--btn').click(nav);
-  $('#fullpage').fullpage({
-		anchors:['about', 'fehandbags', 'eldorado'],
-    navigation: true,
-    navigationPosition: 'left'
-	});
+	$('.menu--btn').on('click', nav);
+	$('.menu-item').on('click', function() {
+    nav()
+    $('html,body')
+      .animate(
+        { scrollTop: $(`#${$(this).attr('id')}-section`).offset().top },
+        'slow'
+      );
+  });
 });
